8e4489806763566b3f7400a0d5b7a7b8e8eb56fd
Eundms <dms011221@gmail.com>
2021-01-17 12:35:16 +0900

    서버-클라이언트

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import Paper from '@material-ui/core/Paper';
+import Customer from './components/Customer';
+
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -8,9 +10,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
-import Customer from './components/Customer';
 
-const styles=theme=>({
+const styles = theme => ({
 root:{
    width: '100%',
    marginTop: theme.spacing.unit*3,
@@ -19,25 +20,29 @@ root:{
 table:{
   minWidth:1080
 }
-})
-const customers=[{
-  'id':1,
-  'image':'https://placeimg.com/64/64/1',
-  'name':'박은정',
-  'birthday':'990211',
-  'gender':'여',
-  'job':'개발자',
-},
-{
-  'id':2,
-  'image':'https://placeimg.com/64/64/2',
-  'name':'정',
-  'birthday':'950211',
-  'gender':'여',
-  'job':'개발자2',
-}];
-
+});
+//고객 데이터를 서버에 접속해서 가져올 수 있어야 함.
+//비동기적으로 값을 가져오기 때문에 처음에 웹 서비스 구동된 상태에서는 customer가 비어있음.
+//this.state.customers가 true값일 때만
 class App extends React.Component{
+  //변경할 수 있는 데이터
+  state={
+      customers:''
+  }
+  //모든 컴포넌트가 마운트가 완료되었을 때 실행되는 것
+  // 이곳에서 api를 불러와야 함.
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
+  }
+  callApi = async()=>{
+    //접속하고자 하는 api 주소(/api/customers)를 response에 넣는다.
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+  //props는 변경 불가한 데이터
   render(){
     const {classes}=this.props;
       return (
@@ -51,12 +56,14 @@ class App extends React.Component{
             <TableCell>생년월일</TableCell>
             <TableCell>성별</TableCell>
             <TableCell>직업</TableCell>
-
           </TableRow>
         </TableHead>
         <TableBody>
-      {customers.map(c=>{return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
-        </TableBody>
+      {this.state.customers? 
+      this.state.customers.map(c=>{
+        return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> )
+      })  : ''}  
+      </TableBody>
       </Table>
      
     </Paper>
